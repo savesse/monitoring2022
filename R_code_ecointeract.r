ea4a1f1c90259494f728e6458c17eaa88b0b05f8
savesse <114755793+savesse@users.noreply.github.com>
2022-10-13 18:00:54 +0200

    Create R_code_ecointeract.r

---
+++ R_code_ecointeract.r
@@ -0,0 +1,5 @@
+# This is a code for investigating relationships among ecological variables
+
+# sp package usage with function install.packages("sp"), here with "" bc we are going out of R for reaching that package; once it's installed I can avoid ""
+# install.packages(sp)
+library(sp)
