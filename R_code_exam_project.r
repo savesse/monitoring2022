8d5d531a318d460afcc1d7fb1ce25403ccc8c1e6
savesse <114755793+savesse@users.noreply.github.com>
2023-02-14 09:39:00 +0100

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -20,31 +20,37 @@ coordinates: List (1 element)         # the list of the 4 groups of coordinates
 geodesic: false
 
 var dataset = ee.ImageCollection('MODIS/006/MOD10A1')                         # we are considering the images coming from MODIS, an instrument onboard a satellite
-                  .filter(ee.Filter.date('2020-01-01', '2020-12-31'));        # we set the time period in which we are intrested, so the year
-var snowCover = dataset.select('NDSI_Snow_Cover');
+                  .filter(ee.Filter.date('2020-01-01', '2020-12-31'));        # we set the time period in which we are intrested, so the single year
+var snowCover = dataset.select('NDSI_Snow_Cover');                            # as a variable we have the snowCover, which is considering the NDSI dataset
+                                                                              # NDSI (normalized difference snow index), useful for the snow and ice
 var snowCoverVis = {
   min: 0.0,
   max: 100.0,
   palette: ['black', '0dffff', '0524ff', 'ffffff'],
-};
+};                                                    # we have also the visualization of the NDSI throughout the snowCoverVis with its palette
 
-print('snowCover', snowCover)
+print('snowCover', snowCover)                 # that's the variable in which we are intrested in
 
-Map.setCenter(11.65, 46.386, 6);
-Map.addLayer(snowCover, snowCoverVis, 'Snow Cover');
+Map.setCenter(11.65, 46.386, 6);              # this is useful in Google Earth Engine to set the centre of the map in a point of our preference
+Map.addLayer(snowCover, snowCoverVis, 'Snow Cover');       # these are the layers present in our map
 
 
-// Export a cloud-optimized GeoTIFF.
-Export.image.toDrive({
-  image: snowCover.select('NDSI_Snow_Cover').mean(),
-  description: 'snowCover Mar 2020',
-  scale: 30,
-  region: geometry,
-  fileFormat: 'GeoTIFF',
+// Export a cloud-optimized GeoTIFF.          # we also need to export this file as a GeoTIFF
+Export.image.toDrive({                          # we do it in the drive
+  image: snowCover.select('NDSI_Snow_Cover').mean(),    # we want the mean of the year for what regards the snow cover
+  description: 'snowCover Mar 2020',          # this is the name given to the GeoTIFF
+  scale: 30,                              # this is the resolution of the scale
+  region: geometry,                     # this is the region which is going to be exported, so our geometry
+  fileFormat: 'GeoTIFF',              # the format
   formatOptions: {
-    cloudOptimized: true
+    cloudOptimized: true            # we want to optimize the images without considering the images with a lot of clouds
   }
 });
+
+
+# Once I got the GeoTIFF files I was able to download them and to visualize them in QGIS
+# Then I started my analysis in R
+
 
 
 
